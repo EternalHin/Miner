907883d9909f9f60714e446c7a816a5da01f2e9d
Salam <1234567>
2020-12-18 19:16:34 +0300

    Miner [1.0.2]
    -Изменение размера окна

--- Miner/MainForm.Designer.cs
+++ Miner/MainForm.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(443, 499);
+            this.ClientSize = new System.Drawing.Size(462, 453);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "MainForm";
