ea871df88e009e0c5ea1910a0d098b1fc860fad7
Salam <1234567>
2020-12-18 19:14:04 +0300

    Miner [1.0.1]

--- Miner/MainForm.Designer.cs
+++ Miner/MainForm.Designer.cs
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(443, 499);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "MainForm";
+            this.Text = "Miner";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
