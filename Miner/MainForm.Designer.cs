7ac7714f8f69a2db024d8f2450eb3b6805eb240f
Maks <Maks@DESKTOP-9NKM007>
2021-01-22 19:48:16 +0300

    Miner[Finished]

--- Miner/MainForm.Designer.cs
+++ Miner/MainForm.Designer.cs
@@ -29,23 +29,38 @@
         private void InitializeComponent()
         {
             this.panelMain = new System.Windows.Forms.Panel();
+            this.ButtonReset = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // panelMain
             // 
             this.panelMain.Location = new System.Drawing.Point(12, 12);
+            this.panelMain.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panelMain.Name = "panelMain";
-            this.panelMain.Size = new System.Drawing.Size(438, 381);
+            this.panelMain.Size = new System.Drawing.Size(400, 400);
             this.panelMain.TabIndex = 0;
+            // 
+            // ButtonReset
+            // 
+            this.ButtonReset.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.ButtonReset.Location = new System.Drawing.Point(312, 419);
+            this.ButtonReset.Name = "ButtonReset";
+            this.ButtonReset.Size = new System.Drawing.Size(100, 30);
+            this.ButtonReset.TabIndex = 1;
+            this.ButtonReset.Text = "Перезапуск";
+            this.ButtonReset.UseVisualStyleBackColor = true;
+            this.ButtonReset.Click += new System.EventHandler(this.buttonReset_Click);
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(462, 453);
+            this.ClientSize = new System.Drawing.Size(424, 461);
+            this.Controls.Add(this.ButtonReset);
             this.Controls.Add(this.panelMain);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "MainForm";
             this.Text = "Miner";
             this.ResumeLayout(false);
@@ -55,6 +70,7 @@
         #endregion
 
         private System.Windows.Forms.Panel panelMain;
+        private System.Windows.Forms.Button ButtonReset;
     }
 }
 
--- Miner/MainForm.cs
+++ Miner/MainForm.cs
@@ -18,22 +18,30 @@ namespace miner
             Miner.Init(this, panelMain);
         }
 
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            Miner.Init(this, panelMain);
+        }
+
+    }
         public static class Miner
         {
-            private const int mapSize = 12;
+            public const int mapSize = 12;
             public const int cellSize = 50;
             public static int[,] map = new int[mapSize + 2, mapSize + 2];
             public static Button[,] buttons = new Button[mapSize + 2, mapSize + 2];
+            public static int[,] currentPictureToSet = new int[mapSize + 2, mapSize + 2];
+            public static int number;
+
+            public static Image sprite;
+            public static bool firstStep = true;
 
             public static Form mainForm;
             public static Panel panelMain;
 
-            public static int number;
-            public static bool firstStep = true;
-
-
             public static void Init(Form tempForm, Panel tempPanel)
             {
+                sprite = new Bitmap(@"Sprite\Sprite.png");
                 mainForm = tempForm;
                 panelMain = tempPanel;
                 firstStep = true;
@@ -56,10 +64,12 @@ namespace miner
                 {
                     for (int j = 0; j <= mapSize; j++)
                     {
-                        map[i, j] = 0;
+                        map[i, j] = 10;
+                        currentPictureToSet[i, j] = 0;
                     }
                 }
             }
+
 
             public static void InitButtons(Panel panelMain)
             {
@@ -71,6 +81,7 @@ namespace miner
                         Button button = new Button();
                         button.Location = new Point(j * cellSize, i * cellSize);
                         button.Size = new Size(cellSize, cellSize);
+                        button.BackgroundImage = FindNeedImage(1, 4);
                         button.MouseUp += new MouseEventHandler(OnButtonPressMouse);
                         panelMain.Controls.Add(button);
                         buttons[i + 1, j + 1] = button;
@@ -80,24 +91,83 @@ namespace miner
 
             public static void OnButtonPressMouse(object sender, MouseEventArgs e)
             {
-                Button pressButton = sender as Button;
+                Button pressedButton = sender as Button;
+
                 switch (e.Button.ToString())
                 {
                     case "Right":
-
+                        OnRightButtonPressed(pressedButton);
                         break;
                     case "Left":
-                        OnLeftButtonPress(pressButton);
+                        OnLeftButtonPress(pressedButton);
+                        break;
+                }
+                if (CheckWin())
+                {
+                    MessageBox.Show("Победа!");
+                    Init(mainForm, panelMain);
+                }
+
+            }
+
+            public static bool CheckWin()
+            {
+                int countMin = 0;
+                int countFlag = 0;
+                for (int i = 1; i <= mapSize; i++)
+                {
+                    for (int j = 1; j <= mapSize; j++)
+                    {
+                        if (map[i, j] == -1 && currentPictureToSet[i, j] == 1)
+                            countMin++;
+                        if (currentPictureToSet[i, j] != 0)
+                            countFlag++;
+                    }
+                }
+
+                return countMin == number && countFlag == countMin;
+            }
+
+            public static void OnRightButtonPressed(Button pressedButton)
+            {
+                int x = pressedButton.Location.Y / cellSize + 1;
+                int y = pressedButton.Location.X / cellSize + 1;
+
+                currentPictureToSet[x, y]++;
+                currentPictureToSet[x, y] %= 3;
+                switch (currentPictureToSet[x, y])
+                {
+                    case 0:
+                        pressedButton.BackgroundImage = FindNeedImage(1, 4);
+                        break;
+                    case 1:
+                        pressedButton.BackgroundImage = FindNeedImage(2, 0);
+                        break;
+                    case 2:
+                        pressedButton.BackgroundImage = FindNeedImage(2, 2);
                         break;
                 }
             }
+
+            public static Image FindNeedImage(int xPos, int yPos)
+            {
+                Image temp = new Bitmap(cellSize, cellSize);
+                Graphics graph = Graphics.FromImage(temp);
+                graph.DrawImage(sprite,
+                    new Rectangle(new Point(0, 0), new Size(cellSize, cellSize)),
+                        0 + 32 * yPos, 0 + 32 * xPos, 33, 33, GraphicsUnit.Pixel);
+
+                return temp;
+            }
+
+
             public static void OnLeftButtonPress(Button pressedButton)
             {
                 int x = pressedButton.Location.Y / cellSize + 1;
                 int y = pressedButton.Location.X / cellSize + 1;
-                pressedButton.Enabled = false;
                 if (firstStep)
                 {
+                    pressedButton.Enabled = false;
                     SeedMap();
                     firstStep = false;
                 }
@@ -109,16 +179,90 @@ namespace miner
                         for (int j = 1; j <= mapSize; j++)
                         {
                             if (map[i, j] == -1)
-                                buttons[i, j].BackColor = Color.Red;
+                                buttons[i, j].BackgroundImage = FindNeedImage(2, 3);
                         }
                     }
-                    MessageBox.Show("GG");
+                    MessageBox.Show("Игра окончена");
                     Init(mainForm, panelMain);
-                    return;
                 }
 
-                buttons[x, y].Text = CheckMin(x, y).ToString();
+                OpenCells(x, y);
             }
+
+
+
+            public static void OpenCells(int x, int y)
+            {
+                OpenCell(x, y);
+
+                if (map[x, y] > 0)
+                    return;
+
+
+                for (int tX = x - 1; tX <= x + 1; tX++)
+                {
+                    for (int tY = y - 1; tY <= y + 1; tY++)
+                    {
+                        if (!IsInBorder(tX, tY))
+                            continue;
+                        if (!buttons[tX, tY].Enabled)
+                            continue;
+                        if (map[tX, tY] == 0)
+                            OpenCells(tX, tY);
+                        else if (map[tX, tY] > 0)
+                            OpenCell(tX, tY);
+                    }
+                }
+            }
+
+            private static void OpenCell(int i, int j)
+            {
+                buttons[i, j].Enabled = false;
+
+                switch (map[i, j])
+                {
+                    case 1:
+                        buttons[i, j].BackgroundImage = FindNeedImage(0, 1);
+                        break;
+                    case 2:
+                        buttons[i, j].BackgroundImage = FindNeedImage(0, 2);
+                        break;
+                    case 3:
+                        buttons[i, j].BackgroundImage = FindNeedImage(0, 3);
+                        break;
+                    case 4:
+                        buttons[i, j].BackgroundImage = FindNeedImage(0, 4);
+                        break;
+                    case 5:
+                        buttons[i, j].BackgroundImage = FindNeedImage(1, 0);
+                        break;
+                    case 6:
+                        buttons[i, j].BackgroundImage = FindNeedImage(1, 1);
+                        break;
+                    case 7:
+                        buttons[i, j].BackgroundImage = FindNeedImage(1, 2);
+                        break;
+                    case 8:
+                        buttons[i, j].BackgroundImage = FindNeedImage(1, 3);
+                        break;
+                    case -1:
+                        buttons[i, j].BackgroundImage = FindNeedImage(2, 1);
+                        break;
+                    case 0:
+                        buttons[i, j].BackgroundImage = FindNeedImage(0, 0);
+                        break;
+                }
+            }
+
+
+            public static bool IsInBorder(int i, int j)
+            {
+                if (i < 1 || j < 1 || j > mapSize || i > mapSize)
+                    return false;
+
+                return true;
+            }
+
             public static void SeedMap()
             {
                 Random rnd = new Random();
@@ -161,6 +305,10 @@ namespace miner
                 }
                 return count;
             }
+
+
         }
-    }
+
+
+    
 }
